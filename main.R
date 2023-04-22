94d047a5690394f85a2d30b8fe89ef497cb4b5a0
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2023-04-22 11:10:41 -0700

    Mapping 1:3 to white:black now. Generalized the rows/colums/z ranges.

--- main.R
+++ main.R
@@ -1,14 +1,18 @@
 library(lattice)
 
-d <- matrix(sample(c(1, 2, 3), size=8, replace=T))
+nz <- 3
+rows <- 2**3
+cols <- 2**2
+d <- matrix(rep(1:nz, length.out=rows*cols), nrow=rows, ncol=cols)
 
 p <- levelplot(
   d,
   xlab=NULL,
   ylab=NULL,
-  scales=list(x=list(at=NULL),
-              y=list(at=NULL)),
-  col.regions=c("#FFFFFF", "#AAAAAA", "#555555", "#000000"),
+  # scales=list(x=list(at=NULL), y=list(at=NULL)), # no ticks
+  scales=list(x=list(at=1:rows), y=list(at=1:cols)),
+  cuts=nz-1, # to map 1:3 to white:black
+  col.regions=colorRampPalette(colors=c("white", "black"))(nz),
   colorkey=NULL
 )
 print(p)
