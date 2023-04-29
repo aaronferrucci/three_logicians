54682d4983eb07d5dc5231cec1766da48d4a3ffb
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2023-04-28 21:17:01 -0700

    trying to make levelplot do a K-map

--- main.R
+++ main.R
@@ -1,16 +1,16 @@
 library(lattice)
 
 nz <- 3
-rows <- 2**3
-cols <- 2**2
-d <- matrix(rep(1:nz, length.out=rows*cols), nrow=rows, ncol=cols)
+x <- 2**1
+y <- 2**2
+d <- t(matrix(rep(1:nz, length.out=x*y), nrow=x, ncol=y))
 
 p <- levelplot(
   d,
   xlab=NULL,
   ylab=NULL,
   # scales=list(x=list(at=NULL), y=list(at=NULL)), # no ticks
-  scales=list(x=list(at=1:rows), y=list(at=1:cols)),
+  scales=list(x=list(at=1:y, labels=c("AB'", "AB", "A'B", "A'B'")), y=list(at=1:x, labels=c("C'", "C"))),
   cuts=nz-1, # to map 1:3 to white:black
   col.regions=colorRampPalette(colors=c("white", "black"))(nz),
   colorkey=NULL
