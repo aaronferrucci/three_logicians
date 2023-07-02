dbaed507e160e0a1285ea35468930ecc1d4af7d7
Aaron Ferrucci <aaron.ferrucci@gmail.com>
2023-07-02 11:36:38 -0700

    bringing the data frame implementation and the matrix requirement of levelplot into agreement

--- utils.R
+++ utils.R
@@ -1,5 +1,75 @@
 library(dplyr)
+
+# 2-logician scenario
+get_questionAB <- function(index) {
+  dat <- data.frame(
+    A=c(0, 0, 1, 1),
+    B=c(0, 1, 0, 1)
+  )
+  dat$value <- ifelse(bitwAnd(2**(0:3), index), 1, 0)
+  dat$source <- ifelse(dat$value, "yes", "no")
+  dat$y <- factor(ifelse(dat$A == 1, "A:yes", "A:no"), levels=c("A:no", "A:yes"))
+  dat$x <- factor(ifelse(dat$B == 1, "B:yes", "B:no"), levels=c("B:no", "B:yes"))
+  return(dat)
+}
+
+get_jokeAB <- function(index) {
+  dat <- get_questionAB(index)
+
+  ssA <- c()
+  for (row in 1:nrow(dat)) {
+    ssA <- append(ssA, ifelse(length(unique(dat[dat$A == dat$A[row],]$value)) == 1, T, F))
+  }
+  dat$ansA <- ifelse(ssA, dat$value*2-1, 0)
+
+  ansB <- c()
+  for (row in 1:nrow(dat)) {
+    ansB <- append(ansB, case_when(
+      dat$ansA[row] != 0 ~ dat$ansA[row],
+      length(unique(dat[dat$ansA == 0 & dat$B == dat$B[row],]$value)) == 1 ~ dat$value[row]*2-1,
+      TRUE ~ 0
+    ))
+  }
+  dat$ansB <- ansB
+
+  return(dat)
+}
+
+# levelplot prefers a matrix for its input data ('x'), but I like data frames.
+# Solution: produce question/joke data as data frames, convert to matrix for
+# plotting.
+
+# Here's how I used to build the 2-logician matrix (for joke 8, "AND"):
+# m <- matrix(nrow=2, ncol=4, data=c(
+#   -1, -1, # 00
+#   -1, -1, # 01
+#    0, -1, # 10
+#    0,  1  # 11
+# ))
+#
+# ... and here's what the input data looks like for "AND, joke 8:
+# > j$ansA
+# [1] -1 -1  0  0
+# > j$ansB
+# [1] -1 -1 -1  1
+#
+# c(rbind()) slices ansA and ansB into the needed order.
+
+to_matrix <- function(joke) {
+  # I'll handle the 2- and 3-input forms as case statements, probably some
+  # commonality will emerge, then can refactor.
+  m <- case_when(
+    nrow(joke) == 4 ~ matrix(data=c(rbind(joke$ansA, joke$ansB)), nrow=2, ncol=4),
+    # to do: 8
+    T ~ c(0)
+  )
+
+  return(m)
+}
+
+
 # question synthesis by index
+# 3-logician scenario
 get_question <- function(index) {
   dat <- data.frame(
     A=c(0, 0, 0, 0, 1, 1, 1, 1),
